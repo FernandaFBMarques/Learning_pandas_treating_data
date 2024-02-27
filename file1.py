9d6ea345bb53c1b88e0b25993508833ed2584020
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-02-27 18:59:21 -0300

    add 2 in file1.py

--- file1.py
+++ file1.py
@@ -1,3 +1,5 @@
+# AULA 03
+
 import pandas as pd
 
 data_ = pd.read_csv('aluguel.csv', sep=';')
@@ -8,10 +10,23 @@ real_estate_type = data_['Tipo']  # serve para selecionar a variavel desejada
 
 # verifica o tipo da variavel
 print(type(real_estate_type))
+# real_estate_type eh uma variavel do tipo Series
 
 # agrupa todos os tipos de imoveis que tem na Series
 # so coleta primeiro item de cada tipo
 # o argumento inplace faz com que a modificação seja feita direto na variavel
 real_estate_type.drop_duplicates(inplace=True)
 
+print(real_estate_type)
+
+
+# Structuring the visualization
+
+real_estate_type = pd.DataFrame(real_estate_type)
+
+# aplicacao do indice numerado
+real_estate_type.index = range(real_estate_type.shape[0])
+
+# nomeando a 1a coluna de id
+real_estate_type.columns.name = 'id'
 print(real_estate_type)
