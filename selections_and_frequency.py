65672dc49347bfd2ab409edc0c60edef8fc5d913
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-01 18:46:41 -0300

    add 1 in selections_and_frequency.py

---
+++ selections_and_frequency.py
@@ -0,0 +1,18 @@
+# AULA 5
+# Selection and Frequency
+
+# atalho = ctrl + /
+
+# Select properties classified as 'Casa', 'Casa de Condomínio' and 'Casa de Vila'
+# Select properties with an area between 60 and 100 square meters, including the limits
+# Select properties with at least 4 bedrooms and with rent less than R$ 2,000.00
+
+import pandas as pd
+
+data = pd.read_csv('aluguel.csv', sep=';')
+print(data.head(10))
+
+# Select only properties classified as 'Apartamento':
+selection = data['Tipo'] == 'Apartamento'
+n1 = data[selection].shape[0]
+
