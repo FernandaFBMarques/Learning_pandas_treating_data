0f6d38fa9327a4c18b2c760fb8b84e5820fbb2f4
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-07 07:48:57 -0300

    add 1 in students.py

---
+++ students.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+students = pd.DataFrame({'Name': ['Ary', 'Cátia', 'Denis', 'Beto', 'Bruna', 'Dara', 'Carlos', 'Alice'],
+                        'Genre': ['M', 'F', 'M', 'M', 'F', 'F', 'M', 'F'],
+                         'Age': [15, 27, 56, 32, 42, 21, 19, 35],
+                         'Grades': [7.5, 2.5, 5.0, 10, 8.2, 7, 6, 5.6],
+                         'Approved': [True, False, False, True, True, True, False, False]},
+                        columns=['Name', 'Age', 'Genre', 'Grades', 'Approved'])
+print(students.head(10))
+
