048aa877c760d3d299f1d80b3b69110bf2286131
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-08 18:06:33 -0300

    add 2 creating_new_variables.py.py

--- creating_new_variables.py
+++ creating_new_variables.py
@@ -9,6 +9,23 @@ print(data.head(10))
 data['Valor m2'] = (data['Valor']/data['Area']).round(2)
 print(data.head(10))
 
+data['Valor Bruto m2'] = (data['Valor Bruto']/data['Area']).round(2)
+
 casa = ['Casa', 'Casa de Condomínio', 'Casa de Vila']
 data['Tipo Agregado'] = data['Tipo'].apply(lambda x: 'Casa' if x in casa else 'Apartamento')
 print(data.head(10))
+
+# Deleating variables
+data_aux = pd.DataFrame(data[['Tipo Agregado', 'Valor m2', 'Valor Bruto', 'Valor Bruto m2']])
+print(data_aux.head(10))
+
+# del data_aux['Valor Bruto']
+# print(data_aux.head(10))
+
+# data_aux.pop('Valor Bruto m2')
+# print(data_aux.head(10))
+
+data.drop(['Valor Bruto', 'Valor Bruto m2'], axis=1, inplace=True)
+print(data_aux.head(10))
+
+data.to_csv('aluguel_residencial.csv', sep=';', index=False)
