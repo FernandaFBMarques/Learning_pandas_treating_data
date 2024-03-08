2aca67b3515137cb0ac126be8857e2f676237ee1
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-08 16:59:43 -0300

    add 2 in treating_missing_data.py

--- treating_missing_data.py
+++ treating_missing_data.py
@@ -8,3 +8,20 @@ A = data.shape[0]
 data.dropna(subset=['Valor'])
 B = data.shape[0]
 print(A-B)
+
+print(data[data['Condominio'].isnull()].shape[0])
+
+selection = (data['Tipo'] == 'Apartamento') & (data['Condominio'].isnull())
+
+A = data.shape[0]
+data = data[~selection]
+B = data.shape[0]
+print(A-B)
+
+print(data[data['Condominio'].isnull()].shape[0])
+
+data.fillna(0, inplace=True)
+# Option 2: data = data.fillna({'Condominio': 0, 'IPTU': 0})
+print(data.info())
+
+data.to_csv('residential_rent.csv', sep=';', index=False)
