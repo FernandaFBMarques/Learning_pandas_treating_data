7266c67a2aaea64d30495657a55865a2f2803b74
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-08 17:17:42 -0300

    add 3 in treating_missing_data_2.py

---
+++ treating_missing_data_2.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+real_estate = pd.DataFrame([['Apartment', None, 970, 68],
+                            ['Apartment', 2000, 878, 112],
+                            ['House', 5000, None, 500],
+                            ['Apartment', None, 1010, 170],
+                            ['Apartment', 1500, 850, None],
+                            ['House', None, None, None],
+                            ['Apartment', 2000, 878, None],
+                            ['Apartment', 1550, None, 228],
+                            ['Apartment', 2500, 880, 195]],
+                           columns=['Type', 'Value', 'Condominium', 'Tax'])
+
+real_estate.dropna(subset=['Value'], inplace=True)
+print(real_estate)
+
+real_estate = real_estate.fillna({'Condominium': 0, 'Tax': 0})
+print(real_estate)
+
+real_estate.index = range(real_estate.shape[0])
+print(real_estate)
