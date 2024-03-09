541d4f76d3e94bdd1f984bb69f590c0322208e94
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-03-09 17:54:49 -0300

    add 2 in creating_groups.py

--- creating_groups.py
+++ creating_groups.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('residential_rent.csv', sep=';')
 print(data.head(10))
@@ -27,3 +28,20 @@ print(groups_neighborhoods_mean)
 
 groups_neighborhoods_mean = groups_neighborhoods[['Valor', 'Condominio']].mean().round(2)
 print(groups_neighborhoods_mean)
+
+print(groups_neighborhoods['Valor'].describe().round(2))
+
+print(groups_neighborhoods['Valor'].aggregate(['min', 'max', 'sum']).rename(columns={'min': 'Minimum',
+                                                                                     'max': 'Maximum',
+                                                                                     'sum': 'Sum'}))
+
+plt.rc('figure', figsize=(20, 10))
+fig = groups_neighborhoods['Valor'].std().plot.bar(color='blue')
+fig.set_ylabel('Rent Value')
+fig.set_title('Rent Value by Neighborhood', {'fontsize': 22})
+plt.show()
+
+fig2 = groups_neighborhoods['Valor'].mean().plot.bar(color='orange')
+fig2.set_ylabel('Rent Value')
+fig2.set_title('Average Rent Value by Neighborhood', {'fontsize': 22})
+plt.show()
